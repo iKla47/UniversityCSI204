646b4e2415ca24d5972cc50f21655cc3bf5a7778
iKla <contact@ikla.net>
2026-06-14 01:09:57 +0700

    อัพเดทหน้าโหลด

--- frontend/src/page/index.tsx
+++ frontend/src/page/index.tsx
@@ -4,20 +4,20 @@
 import react from "react";
 import reactDom from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+//
+// โหลดหน้าต่างเมื่อจำเป็นเท่านั้น
+//
+const Init = react.lazy (() => import ("./init.tsx"));
+const Auth = react.lazy (() => import ("./auth.tsx"));
+const Home = react.lazy (() => import ("./home.tsx"));
+const Settings = react.lazy (() => import ("./settings.tsx"));
 
 
-const view = document.getElementById ("app") as HTMLDivElement;
-const root = reactDom.createRoot (view);
 
 function Main ()
 {
-  //
-  // โหลดหน้าต่างเมื่อจำเป็นเท่านั้น
-  //
-  const Init = react.lazy (() => import ("./init.tsx"));
-  const Auth = react.lazy (() => import ("./auth.tsx"));
-  const Home = react.lazy (() => import ("./home.tsx"));
-  const Settings = react.lazy (() => import ("./settings.tsx"));
+  const view = document.getElementById ("app") as HTMLDivElement;
+  const root = reactDom.createRoot (view);
 
   root.render (<>
     <react.StrictMode>
@@ -37,7 +37,24 @@ function Main ()
 }
 export default function Splash ()
 {
+  const root: react.CSSProperties =
+  {
+    position: "absolute", inset: "0px",
+    width: "100%", height: "100%",
+    backgroundColor: "rgb(16, 16, 24)"
+  };
+  const center: react.CSSProperties =
+  {
+    position: "absolute", inset: "0px",
+    display: "flex", flexDirection: "column",
+    alignItems: "center", justifyContent: "center"
+  };
+
   return <>
+    <div style={root}/>
+    <div style={center}>
+      <h1>The Web Project</h1>
+    </div>
   </>;
 }
 
