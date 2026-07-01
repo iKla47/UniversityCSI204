47fdddc8e8a4470c972e9869a132322cf28bc509
iKla47 <67828086+iKla47@users.noreply.github.com>
2026-07-01 11:03:39 +0700

    Updates

---
+++ UniversityCSI204/product-B7PNJrHC.js
@@ -0,0 +1,23 @@
+import{t as e}from"./jsx-runtime-DGeXAQPT.js";import{t}from"./styled-components.browser.esm-DY3kMFV8.js";var n=e(),r=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsxs)(i,{children:[(0,n.jsx)(r.HeadImage,{}),(0,n.jsx)(r.HeadIntro,{})]})})};r.HeadImage=function(){return(0,n.jsx)(a,{$width:`40%`,children:(0,n.jsx)(`img`,{src:void 0,width:256,height:256})})},r.HeadIntro=function(){return(0,n.jsxs)(a,{$width:`60%`,children:[(0,n.jsx)(o,{children:`[PRODUCT TITLE]`}),(0,n.jsxs)(s,{children:[(0,n.jsx)(`label`,{children:`[PRODUCT VENDOR]`}),(0,n.jsx)(`label`,{children:`*`}),(0,n.jsx)(`label`,{children:`[PRODUCT PRICE]`})]}),(0,n.jsx)(c,{children:`เพิ่มลงในตะกรา`})]})};var i=t.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  padding: 48px 256px;
+`,a=t.div`
+  width: ${e=>e.$width};
+  height: 100%;
+`,o=t.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  padding-bottom: 8px;
+`,s=t.div`
+  width: 100%;
+  height: 24px;
+
+  * { padding-right: 8px; }
+`,c=t.button`
+  width: 256px;
+`;export{r as default};
