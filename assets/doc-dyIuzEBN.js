9d6b6bf1c32b5928d1cfb4ddc61729078b405fd9
iKla47 <67828086+iKla47@users.noreply.github.com>
2026-07-01 11:30:34 +0700

    Updates

---
+++ assets/doc-dyIuzEBN.js
@@ -0,0 +1,15 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/doc-Di4wxMeb.js","assets/jsx-runtime-DGeXAQPT.js"])))=>i.map(i=>d[i]);
+import{r as e,t}from"./jsx-runtime-DGeXAQPT.js";import{i as n,o as r,r as i,s as a}from"./index-H8fASI2Q.js";import{t as o}from"./styled-components.browser.esm-Xzin27X1.js";var s=e(a(),1),c=t(),l=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(n,{children:(0,c.jsx)(i,{children:(0,c.jsx)(i,{path:`/`,index:!0,element:(0,c.jsx)((0,s.lazy)(()=>r(()=>import(`./doc-Di4wxMeb.js`),__vite__mapDeps([0,1]))),{})})})})})};l.View=function({children:e}){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(u,{}),(0,c.jsx)(d,{children:(0,c.jsx)(f,{children:(0,c.jsx)(p,{children:e})})})]})};var u=o.div`
+
+`,d=o.div`
+  position: static;
+  inset: 0px;
+  width: 100%;
+  height: 100%;
+`,f=o.div`
+  margin: 0 auto;
+  padding-left: 128px;
+  width: 210mm;
+  height: auto;
+`,p=o.main`
+`;export{l as default};
