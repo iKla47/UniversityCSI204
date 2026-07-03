1f3d112378cf42e87f5c18245dca92b2cad6322b
iKla47 <67828086+iKla47@users.noreply.github.com>
2026-07-03 20:17:50 +0700

    Updates

---
+++ assets/home-ezo-GzoC.js
@@ -0,0 +1,44 @@
+import{t as e}from"./index-CBWIj0JZ.js";import{t}from"./styled-components.browser.esm-B2zWX8Nk.js";var n=`/UniversityCSI204/assets/home.testImage-Biv9CYx4.jpg`,r=e(),i=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsxs)(a,{children:[(0,r.jsx)(o,{src:n}),(0,r.jsxs)(s,{children:[(0,r.jsx)(`h1`,{children:`สวัสดีชาวออกซิเจน`}),(0,r.jsx)(`p`,{children:`เวอร์เซิร์ฟเวอร์ 00:00:00`})]})]})})},a=t.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  background-color: yellow;
+  position: relative;
+`,o=t.img`
+  display: block;
+  position: absolute;
+  inset: 0px;
+  border-radius: 8px;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+`,s=t.div`
+  width: 100%;
+  height: 100%;
+  inset: 0px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: start;
+  justify-content: center;
+  padding: 64px;
+
+  h1
+  {
+    color: red;
+    font-size: 2.5rem;
+  }
+  p
+  {
+    color: red;
+    font-size: 1.5rem;
+  }
+`,c=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(c.Intro,{})})};c.Intro=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(l,{children:(0,r.jsx)(u,{children:(0,r.jsx)(i,{})})})})};var l=t.div`
+  width: 100dvw;
+  height: 100dvh;
+`,u=t.div`
+  width: 100%;
+  height: 100%;
+  padding: 64px;
+`,d=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(c.Intro,{})})};export{d as default};
--- assets/settings-B0NrfXvE.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-DGeXAQPT.js";var t=e();function n(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(`h1`,{children:`Hello There!`})})}export{n as default};
